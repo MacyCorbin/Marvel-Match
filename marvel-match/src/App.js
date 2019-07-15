6c47045202fe85a71d78a7bd4a03e225f1c88167
Macy Corbin <macycorbin@Macys-MacBook-Pro.local>
2019-07-15 12:40:02 -0400

    added components

--- marvel-match/src/App.js
+++ marvel-match/src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import cards from "./cards.json";
+import "./App.css";
 
 function App() {
   return (
