0cda93ec1a9aed1fc76c3cf75f1108adc42f2e90
Macy Corbin <macycorbin@Macys-MacBook-Pro.local>
2019-07-15 20:44:45 -0400

    removed a file not needed

--- marvel-match/src/App.js
+++ marvel-match/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Card from "./components/Card";
-import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import cards from "./cards.json";
